e0cefa729986bb06c57d7883034a8341119aefee
Junior <junior@nois3lab.it>
2012-07-03 00:34:46 +0200

    bug fixing

--- semantic-engine/semanthic-engine.php
+++ semantic-engine/semanthic-engine.php
@@ -10,50 +10,28 @@ Version: 0.1alfa
 /** execute main query only once and then retrieve n times with global $semantic_CPT **/
 $semantic_CPT = $wpdb->get_results("SELECT * FROM ".$wpdb->prefix ."semantic_engine_CPT");
 /**add action area**/
-add_action('admin_menu', 'semantic_engine_admin_menu');
 add_action('admin_init', 'semantic_engine_admin_init');
-add_action('save_post', 'semantic_engine_save_postdata');
 add_action('init', 'semantic_engine_init');
 add_action('pre_get_posts', 'semantic_pre_get_posts');
+add_action('save_post', 'semantic_engine_save_postdata');
+add_action('admin_menu', 'semantic_engine_admin_menu');
 /** path define **/
 define('SEMANTIC_ENGINE_PATH', plugin_dir_path(__FILE__));
 define('SEMANTIC_ENGINE_URL',  plugin_dir_url(__FILE__));
 /** END of setup zone **/
+function semantic_engine_init() {
+	//sql db install and updates
+	$semantic_engine_db_version = "0.1";
+	if(get_option('semantic_engine_db_version') != $semantic_engine_db_version)
+		semantic_engine_sql_install();
 
-function semantic_engine_admin_menu() {
-	add_menu_page('Semantic Engine Dashboard', 'Semantic Engine', 'manage_options', 'semantic-engine-dashboard', 'semantic_engine_admin_dashboard_build', SEMANTIC_ENGINE_URL.'/icons/icon_menu.png', 61);
-	add_submenu_page('semantic-engine-dashboard', 'Add new content type', 'Add Content Type', 'manage_options', 'semantic-engine-new-content-type', 'semantic_engine_add_content_type');
-	add_submenu_page('semantic-engine-dashboard', 'Add new view mode', 'Add View Mode', 'manage_options', 'semantic-engine-new-view-mode', 'semantic_engine_add_view_mode');
+	semantic_engine_CPT_setup();
 
-}
-
-function semantic_engine_admin_dashboard_build() {
-	global $wpdb, $semantic_CPT;
-	if(isset($_GET['cpt']) && !empty($_GET['cpt'])) {
-		switch ($_GET['action']) {
-			case 'custom_fields':
-					$table_name = $wpdb->prefix . "semantic_engine_CPT";
-					$semantic_CPT = $wpdb->get_row( "SELECT title, active FROM $table_name WHERE ID = ".$_GET['cpt']);
-					$table_name = $wpdb->prefix . "semantic_engine_CF";
-					$semantic_CF = $wpdb->get_results( "SELECT * FROM $table_name WHERE id_cpt = ".$_GET['cpt']);
-					include_once(SEMANTIC_ENGINE_PATH.'/admin/admin_new_custom_fields.php');
-				break;
-			
-			default:
-				# code...
-				break;
-		}
-	} else {
-	
-	include_once(SEMANTIC_ENGINE_PATH.'/admin/admin_dashboard.php');
-	}
-}
-function semantic_engine_add_content_type() {
-	//new custom post type
-	include_once(SEMANTIC_ENGINE_PATH.'/admin/admin_new_cpt.php');
 }
 
 function semantic_engine_admin_init() {
+		global $wpdb, $semantic_CPT;
+	include(SEMANTIC_ENGINE_PATH.'admin-function.php');
 	//post submit new CPT
 	if(isset($_POST['semantic_post_submit'])) {
 		switch ($_POST['semantic_post_submit']) {
@@ -67,12 +45,18 @@ function semantic_engine_admin_init() {
 				# code...
 				break;
 		}
-		
 	}
+	wp_enqueue_script('semantic-image', SEMANTIC_ENGINE_URL.'js/custom.js');
+}
+
+function semantic_engine_admin_menu() {
+	add_menu_page('Semantic Engine Dashboard', 'Semantic Engine', 'manage_options', 'semantic-engine-dashboard', 'semantic_engine_admin_dashboard_build', SEMANTIC_ENGINE_URL.'/icons/icon_menu.png', 61);
+	add_submenu_page('semantic-engine-dashboard', 'Add new content type', 'Add Content Type', 'manage_options', 'semantic-engine-new-content-type', 'semantic_engine_add_content_type');
+	add_submenu_page('semantic-engine-dashboard', 'Add new view mode', 'Add View Mode', 'manage_options', 'semantic-engine-new-view-mode', 'semantic_engine_add_view_mode');
+
 }
 
 function semantic_engine_sql_install() {
-
 	global $wpdb;
 	require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
 	$sql = "CREATE TABLE `".$wpdb->prefix."semantic_engine_CPT` (
@@ -103,132 +87,61 @@ function semantic_engine_sql_install() {
 
 }
 
-function semantic_engine_insert_new_CPT() {
-	$title = $_POST['title_field'];
-	$title_sing = $_POST['title_field_sing'];
-	$slug = $_POST['slug'];
-	$front_base = $_POST['with_frontbase'];
-	$exclude_from_search = $_POST['exclude_from_search'];
-	$admin_position = $_POST['admin_position'];
-	$show_in_toolbar = $_POST['show_in_toolbar'];
-	$active = 0;
-
-	global $wpdb;
-	$table_name = $wpdb->prefix . "semantic_engine_CPT";
-	$rows_affected = $wpdb->insert( $table_name, array( 'id' => null, 'title' => $title, 'title_sing' => $title_sing, 'slug' => $slug, 'front_base' => $front_base, 'exclude_from_search' => $exclude_from_search, 'position' => $admin_position, 'show_in_toolbar' => $show_in_toolbar, 'active' => $active) );
-
-	wp_redirect('admin.php?page=semantic-engine-dashboard');
-}
-
-function semantic_engine_insert_new_CF() {
-	$title = $_POST['title_field'];
-	$widget = $_POST['widget'];
-	$values = $_POST['values'];
-	$multiple_entries = $_POST['multiple_entries'];
-	$id_cpt = $_POST['id_cpt'];
-
-	global $wpdb;
-	$table_name = $wpdb->prefix . "semantic_engine_CF";
-	$rows_affected = $wpdb->insert( $table_name, array( 'id' => null, 'title' => $title, 'widget' => $widget, 'csv_values' => $values, 'multiple_entries' => $multiple_entries, 'id_cpt' => $id_cpt) );
-
-	wp_redirect('admin.php?page=semantic-engine-dashboard&cpt='.$id_cpt.'&action=custom_fields');
-}
-
-function semantic_engine_init() {
-
-	//sql db install and updates
-	$semantic_engine_db_version = "0.1";
-	if(get_option('semantic_engine_db_version') != $semantic_engine_db_version)
-		semantic_engine_sql_install();
-
-	semantic_engine_CPT_setup();
-
-}
 function semantic_engine_meta_box_cb($post_obj) {
 	add_meta_box('id'.$post_obj->post_type, translate('Custom fields'), 'semantic_engine_build_metabox', $post_obj->post_type, 'normal', 'high'); 
 }
+
 function semantic_engine_build_metabox($post) {
 	global $wpdb;
 	$cpt_id = str_replace('semantic_', '', $post->post_type);
 	$table_name = $wpdb->prefix . "semantic_engine_CF";
 	$semantic_CF = $wpdb->get_results( "SELECT * FROM $table_name WHERE id_cpt = '$cpt_id'");
 	foreach($semantic_CF as $cf_row):
+		/**JavaScript print for multiple entries support*/ ?>
+		<script type="text/javascript">
+		function addNewEntry(labelInput) {
+			var cosa = jQuery("input#id_"+labelInput).filter(':last').after('<br/><label for="id_'+labelInput+'"> '+labelInput+'</label> <input type="text" name="'+labelInput.toLowerCase()+'[]" id="id_'+labelInput+'" value="" />');
+		}
+		</script>
+	<?php
 		echo '<div style="margin-top: 8px;">';
 		semantic_admin_html_widget($cf_row->title, $cf_row->csv_values, $cf_row->widget, $post->ID, $cf_row->ID,$cf_row->multiple_entries);
 		echo'<input type="hidden" name="semantic_engine_noncename" id="semantic_engine_noncename_noncename" value="'.wp_create_nonce( plugin_basename(__FILE__) ).'" />';
 		echo '</div>';
 	endforeach;
 }
-function semantic_admin_html_widget($label, $values, $widget, $post, $cf_id, $allow_multiple) {
-	switch($widget) {
-		case 1:
-		#input text
-			if(!$allow_multiple) {
-				$val = get_post_meta($post, strtolower($label).$cf_id, true);
-				echo '<label for="id_'.$label.'"> '.$label.'</label> <input type="text" name="'.strtolower($label).'" id="id_'.$label.'" value="'.$val.'" />';
-			} else {
-				$val = get_post_meta($post, strtolower($label).$cf_id, true);
-				echo '<label for="id_'.$label.'"> '.$label.'</label> <input type="text" name="'.strtolower($label).'[]" id="id_'.$label.'" value="'.$val.'" />';
-				echo '<br/><a style="cursor:pointer;" onClick="addNewEntry(\''.strtolower($label).'\');">Add new entry for '.$label.'</a>';
-			} 
-			break;
-		case 2:
-		#checkbox
-			echo '<label>'.$label.'</label> &nbsp;';
-				$values = explode(',', $values);
-				foreach($values as $vl) {
-					$vl = trim($vl);
-					$data = get_post_meta($post, strtolower($label).$cf_id, true);
-					$data = explode(",", $data);
-					if(in_array($vl, $data))
-					echo '<input type="checkbox" id="id_'.$vl.'" name="'.strtolower($label).'[]"  value="'.$vl.'" checked="true">&nbsp;<label for="id_'.$vl.'">'.$vl.'</label>&nbsp; &nbsp;';
-					else
-					echo '<input type="checkbox" id="id_'.$vl.'" name="'.strtolower($label).'[]"  value="'.$vl.'">&nbsp;<label for="id_'.$vl.'">'.$vl.'</label>&nbsp; &nbsp;';
-				}
-			break;
-		case 3:
-			echo '<label>'.$label.'</label> &nbsp;';
-				$values = explode(',', $values);
-				foreach($values as $vl) {
-					$vl = trim($vl);
-					if(get_post_meta($post, strtolower($label).$cf_id, true) == $vl)
-					echo '<input type="radio" name="'.strtolower($label).'" id="id_'.$vl.'"  value="'.$vl.'" checked="true">&nbsp;<label for="id_'.$vl.'">'.$vl.'</label>&nbsp; &nbsp;';
-					else
-					echo '<input type="radio" name="'.strtolower($label).'" id="id_'.$vl.'"  value="'.$vl.'" checked="true">&nbsp;<label for="id_'.$vl.'">'.$vl.'</label>&nbsp; &nbsp;';
-				}
-			break;
-		case 4:
-		#select one value
-			echo '<label for="id_'.$label.'">'.$label.'</label> &nbsp;';
-			echo '<select name="'.strtolower($label).'" id="id_'.$label.'">';
-				$values = explode(',', $values);
-				foreach($values as $vl) {
-					$vl = trim($vl);
-					if(get_post_meta($post, strtolower($label).$cf_id, true) == $vl)
-					echo '<option value="'.$vl.'" selected="true">'.$vl.'</option>';
-					else
-					echo '<option value="'.$vl.'">'.$vl.'</option>';
-				}
-			echo '</select>';
-			break;
-		case 5:
-		#select multiple value
-			echo '<label for="id_'.$label.'">'.$label.'</label> &nbsp;';
-			echo '<select name="'.strtolower($label).'[]" id="id_'.$label.'" multiple="multiple">';
-				$values = explode(',', $values);
-				foreach($values as $vl) {
-					$vl = trim($vl);
-					$data = get_post_meta($post, strtolower($label).$cf_id, true);
-					$data = explode(",", $data);
-					if(in_array($vl, $data))
-					echo '<option value="'.$vl.'" selected="true">'.$vl.'</option>';
-					else
-					echo '<option value="'.$vl.'">'.$vl.'</option>';
-				}
-			echo '</select>';
-			break;
+
+function semantic_pre_get_posts( $query ) {
+    if ( $query->is_main_query() && ! $query->get( 'post_type' ) )
+        $query->set( 'post_type', array('post', 'semantic_1', 'semantic_2') );
+}
+
+function semantic_engine_CPT_setup() {
+	global $semantic_CPT;
+	foreach($semantic_CPT as $semantic_row_CPT) {
+		register_post_type( 'semantic_'.$semantic_row_CPT->ID,
+		array(
+			'labels' => array(
+				'name'          => $semantic_row_CPT->title,
+				'singular_name' => $semantic_row_CPT->title_sing
+			),
+			'public'               => true,
+			'has_archive'          => true,
+			'exclude_from_search'  => $semantic_row_CPT->exclude_from_search,
+			'show_ui'              => true, 
+			'show_in_menu'         => true, 
+			'show_in_admin_bar'    => $semantic_row_CPT->show_in_toolbar,
+			'menu_position'        => $semantic_row_CPT->position,
+			'supports'             => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments'),
+			'rewrite'              => array('slug' => $semantic_row_CPT->slug),
+			'register_meta_box_cb' => 'semantic_engine_meta_box_cb',
+			'taxonomies'           => array('category', 'post_tag')
+		)
+	);
 	}
 }
+
+
 function semantic_engine_save_postdata($post_id) {
 	global $post;
 	if(!isset($_POST['semantic_engine_noncename']))
@@ -243,16 +156,19 @@ function semantic_engine_save_postdata($post_id) {
 		return $post_id;
 	/**end security check**/
 	global $wpdb;
-	$cpt_id = str_replace('semantic_', '', $_POST['post_type']);
-	$table_name = $wpdb->prefix . "semantic_engine_CF";
+	$cpt_id      = str_replace('semantic_', '', $_POST['post_type']);
+	$table_name  = $wpdb->prefix . "semantic_engine_CF";
 	$semantic_CF = $wpdb->get_results( "SELECT * FROM $table_name WHERE id_cpt = '$cpt_id'");
 	foreach($semantic_CF as $cf_row):
 
-		$data = $_POST[strtolower($cf_row->title)];
+		$data = $_POST[sanitize_title($cf_row->title)];
 		if($cf_row->widget == 2 || $cf_row->widget == 5)
 			$data = implode(',',$data);
 
-		$field_name = strtolower($cf_row->title).$cf_row->ID;
+		if($cf_row->multiple_entries == 1)
+			$data = serialize($data);
+
+		$field_name = sanitize_title($cf_row->title).$cf_row->ID;
 		if(get_post_meta($post_id, $field_name) == "")
 			add_post_meta($post_id, $field_name, $data, true);
 		elseif($data != get_post_meta($post_id, $field_name, true))
@@ -264,32 +180,47 @@ function semantic_engine_save_postdata($post_id) {
 
 }
 
-function semantic_pre_get_posts( $query ) {
-    if ( $query->is_main_query() && ! $query->get( 'post_type' ) )
-        $query->set( 'post_type', array('post', 'page', 'semantic_1') );
+function semantic_engine_view_content($content) {
+	global $post, $wpdb;
+	if(strpos($post->post_type, 'semantic_') !== false):
+		$CPT         = str_replace('semantic_', '', $post->post_type);
+		$table_name  = $wpdb->prefix . "semantic_engine_CF";
+		$semantic_CF = $wpdb->get_results( "SELECT * FROM $table_name WHERE id_cpt = '$CPT'");
+		foreach($semantic_CF as $cf_row):
+			$field_name = sanitize_title($cf_row->title).$cf_row->ID;
+			$val        = get_post_meta($post->ID, $field_name, true);
+			if(!empty($val)):
+				$vl = semantic_engine_get_meta_value_by_type($val, $cf_row->widget, $cf_row->multiple_entries);
+			endif;
+		endforeach;
+	endif;
+	return $content;
 }
 
-function semantic_engine_CPT_setup() {
-	global $semantic_CPT;
-	foreach($semantic_CPT as $semantic_row_CPT) {
-		register_post_type( 'semantic_'.$semantic_row_CPT->ID,
-		array(
-			'labels' => array(
-				'name' => $semantic_row_CPT->title,
-				'singular_name' => $semantic_row_CPT->title_sing
-			),
-			'public' => true,
-			'has_archive' => true,
-			'exclude_from_search' => $semantic_row_CPT->exclude_from_search,
-			'show_ui' => true, 
-    		'show_in_menu' => true, 
-    		'show_in_admin_bar' => $semantic_row_CPT->show_in_toolbar,
-    		'menu_position' => $semantic_row_CPT->position,
-			'supports' => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments'),
-			'rewrite' => array('slug' => $semantic_row_CPT->slug),
-			'register_meta_box_cb' => 'semantic_engine_meta_box_cb',
-			'taxonomies' => array('category', 'post_tag')
-		)
-	);
+function semantic_engine_get_meta_value_by_type($meta, $type, $allow_multiple = null) {
+	switch ($type) {
+		case 1:
+			if(!$allow_multiple)
+				return $meta;
+			else
+				return unserialize($meta);
+			break;
+		case 2:
+				return explode(",", $meta);
+			break;
+		case 3:
+				return $meta;
+			break;
+		case 4:
+				return $meta;
+			break;
+		case 5:
+				return explode(",", $meta);
+			break;
+		default:
+			return $meta;
+			break;
 	}
 }
+add_filter('the_content', 'semantic_engine_view_content', 1);
+
